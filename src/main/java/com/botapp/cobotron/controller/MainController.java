50d4db1ff668123099a9dafeba1b77068832ca09
alejandrorodriguez202 <alejandro@alexdevrs.com>
2025-10-26 12:43:22 -0600

    Version funcional del proyecto

---
+++ src/main/java/com/botapp/cobotron/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.botapp.cobotron.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/v1/cobotron")
+public class MainController {
+
+    @GetMapping("/hello")
+    public static String index() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/botapp/cobotron/CoboTronAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botapp.cobotron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoboTronAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
